3cde22c85d19925c825f302b46ebd2053377ada1
Giorgi Chavchanidze <giorgi@vivaldi.com>
2021-07-17 13:42:35 +0200

    Revised Horn keymap

--- keyboards/idobo/keymaps/gkbd_horn/keymap.c
+++ keyboards/idobo/keymaps/gkbd_horn/keymap.c
@@ -18,19 +18,19 @@
 
 #define MIN_PROXIMITY 8000
 
-#define HORNY(code) (KC_A <= (code) && (code) <= KC_Z && code != KC_T && code != KC_S && code != KC_C && code != KC_P && code != KC_G && code != KC_W && code != KC_X)
+#define PRENASAL(code) (KC_A <= (code) && (code) <= KC_Z && code != KC_T && code != KC_S && code != KC_C && code != KC_P && code != KC_G && code != KC_W && code != KC_X)
 
 uint16_t predecessor_key = KC_NO;
 uint16_t adaptive_key = KC_NO;
 
-static uint16_t horn_proximity;
+static uint16_t prenasal_proximity;
 
 enum custom_keycodes {
     HN = SAFE_RANGE,
 };
 
-bool horny(void) {
-	return predecessor_key == KC_A && timer_elapsed(horn_proximity) < MIN_PROXIMITY && !(get_mods() & ~MOD_MASK_SHIFT & MOD_MASK_CAG);
+bool prenasal(void) {
+	return predecessor_key == KC_A && timer_elapsed(prenasal_proximity) < MIN_PROXIMITY && !(get_mods() & ~MOD_MASK_SHIFT & MOD_MASK_CAG);
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
@@ -39,7 +39,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 		
         if (record->event.pressed) {
 
-			keycode = horny()?KC_N:KC_H;	
+			keycode = prenasal()?KC_N:KC_H;
 			register_code(keycode);
 			adaptive_key = keycode;
 
@@ -49,10 +49,10 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 		}
 	}
 
-	if (!IS_MOD(keycode) && record->event.pressed) {
-		if(HORNY(keycode) || keycode == RSFT_T(KC_H)) {
+	if (!IS_MOD(keycode) && keycode != OSM(MOD_LSFT) && record->event.pressed) {
+		if(PRENASAL(keycode)) {
 			predecessor_key = KC_A;
-			horn_proximity = timer_read();
+			prenasal_proximity = timer_read();
 		}
 		else {
 			predecessor_key = keycode;
@@ -67,8 +67,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 			KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_LBRC, KC_APP, KC_RBRC, KC_6, KC_7, KC_8, KC_9, KC_0, KC_EQL,
 			KC_QUOT, KC_J, KC_X, KC_U, KC_F, KC_W, KC_LCBR, KC_PSCR, KC_RCBR, KC_G, KC_M, KC_P, KC_N, KC_Q, KC_MINS,
 			KC_ESC, KC_R, KC_O, KC_E, HN, KC_L, KC_LPRN, MO(1), KC_RPRN, KC_C, KC_T, KC_I, KC_S, KC_D, KC_ENT,
-			KC_TAB, KC_SCLN, KC_Z, KC_Y, KC_COMM, KC_DOT, KC_SLSH, KC_LGUI, KC_BSLS, KC_K, KC_V, KC_B, RSFT_T(KC_H), KC_UP, KC_INS,
-			KC_CAPS, KC_HOME, KC_END, KC_DEL, KC_BSPC, KC_A, KC_LSFT, KC_RCTL, KC_RALT, KC_SPC, KC_PGDN, KC_PGUP, KC_LEFT, KC_DOWN, KC_RGHT
+			KC_TAB, KC_SCLN, KC_Z, KC_Y, KC_COMM, KC_DOT, KC_SLSH, KC_LGUI, KC_BSLS, KC_K, KC_V, KC_B, KC_H, KC_UP, KC_INS,
+			KC_CAPS, KC_HOME, KC_END, KC_DEL, KC_BSPC, KC_A, OSM(MOD_LSFT), KC_RCTL, KC_RALT, KC_SPC, KC_PGDN, KC_PGUP, KC_LEFT, KC_DOWN, KC_RGHT
 		),
 	[1] = LAYOUT_ortho_5x15(/* Function keys */
 			KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
